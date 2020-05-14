9a4b994d5b8e6c8ec3544dc3c60af33f99e4b9f3
Mankell <mankell.he@gmail.com>
2020-05-14 22:42:28 +0800

    初始化项目目录结构

---
+++ gmall-ums/src/test/java/org/mankell/ums/GmallUmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mankell.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
