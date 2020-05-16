45aa0cc040424933c52cc6ab7897046ae8307f68
Mankell <mankell.he@gmail.com>
2020-05-16 19:09:21 +0800

    测试redis环境，并替换默认redisTemplate

---
+++ gmall-pms/src/main/java/org/mankell/gmall/pms/config/GmallPmsRedisConfig.java
@@ -0,0 +1,21 @@
+package org.mankell.gmall.pms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+import java.net.UnknownHostException;
+
+@Configuration
+public class GmallPmsRedisConfig {
+
+    @Bean("redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+        RedisTemplate<Object, Object> template = new RedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
--- gmall-pms/src/test/java/org/mankell/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/org/mankell/gmall/pms/GmallPmsApplicationTests.java
@@ -1,16 +1,40 @@
 package org.mankell.gmall.pms;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mankell.gmall.pms.entity.Brand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GmallPmsApplicationTests {
 
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate<Object, Object> redisTemplate;
+
     @Test
+
     public void contextLoads() {
+        stringRedisTemplate.opsForValue().set("hello", "world");
+        String hello = stringRedisTemplate.opsForValue().get("hello");
+        log.info("stringRedisTemplate获得的值：{}", hello);
+    }
+
+    @Test
+    public void redisTemplate() {
+        redisTemplate.opsForValue().set("brand4", new Brand().setName("尼玛的"));
+        Brand brand = (Brand) redisTemplate.opsForValue().get("brand4");
+        log.info("redisTemplate获得的值：{}", brand);
     }
 
 }
